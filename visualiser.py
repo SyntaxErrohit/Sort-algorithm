cd7ea4944328f204b4c09efc95f3cf72fbdbd450
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-14 22:39:18 +0530

    made sm code

--- visualiser.py
+++ visualiser.py
@@ -1,3 +1,19 @@
 import pygame
 pygame.init()
 
+display_width = 451
+display_height = 451
+display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("Sort visualiser")
+
+l = [5, 6, 3, 9, 4, 2, 7, 8]
+
+
+
+running = True
+while running:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
